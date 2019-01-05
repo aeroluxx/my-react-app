67c9e0ffca42db98b14716237af1603b9b45f89e
aero <aeroluxx@ya.ru>
2019-01-05 12:57:33 +0300

    first commit
    создал базовый шаблон

---
+++ src/components/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div>
+          <img src='https://www.motto.net.ua/old_site/img/places/1312791996_CDFCFE202D20E9EEF0EA.jpg' alt=''/>
+        </div>
+          <div>
+            ava + description
+          </div>
+          <div>
+            My post
+          </div>
+          <div>
+            New post
+          </div>
+            <div>
+              post#1
+            </div>
+            <div>
+              post#2
+            </div>
+            <div>
+              post#3
+            </div>
+
+      </div>
+            );
+  }
+
+export default Profile;
