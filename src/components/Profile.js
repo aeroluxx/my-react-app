02c7302afb406b1b6c9aa0c26b7f480a61682ae3
aero <aeroluxx@ya.ru>
2019-01-08 22:12:45 +0300

    послеаварийный комит

---
+++ src/components/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+      <div className={s.content}>
+        <div>
+          <img src='https://www.motto.net.ua/old_site/img/places/1312791996_CDFCFE202D20E9EEF0EA.jpg' alt=''/>
+        </div>
+          <div>
+            ava + description
+          </div>
+          <div>
+            My post
+          </div>
+          <div>
+            New post
+          </div>
+            <div>
+              post#1
+            </div>
+            <div>
+              post#2
+            </div>
+            <div>
+              post#3
+            </div>
+
+      </div>
+            );
+  }
+
+export default Profile;
