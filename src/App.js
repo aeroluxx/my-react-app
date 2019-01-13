7a88ae80207a2ffc8e562d48627546fa1b9f7da1
aero <aeroluxx@ya.ru>
2019-01-13 15:32:18 +0300

    новый логотип добавлены новые разделы на все пункты меню в Navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 
@@ -15,6 +18,9 @@ const App = () => {
               <div class='app-wrapper-content'>
                   <Route path='/dialogs' component={Dialogs}/>
                   <Route path='/profile' component={Profile}/>
+                  <Route path='/music' component={Music}/>
+                  <Route path='/news' component={News}/>
+                  <Route path='/settings' component={Settings}/>
               </div>
           </div>
       </BrowserRouter>)
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-          <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' alt=''/>
+          <img src='http://www.boliviaenmovimiento.net/wp-content/uploads/2017/07/Extraordinary-Create-Ur-Own-Logo-46-On-Logo-Designer-with-Create-Ur-Own-Logo.jpg' alt=''/>
         </header>
             );
   }
