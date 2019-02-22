96bde7198fc5799964abb8ba83ca8d1aa331f616
aeroluxx <aeroluxx@ya.ru>
2019-02-22 11:30:48 +0300

    коммит к 23 уроку

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import s from './Dialogs.module.css'
+import React from "react";
+import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = props => {
+  return (
+    <div className={s.dialog + " " + s.active}>
+      <NavLink to={"dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = props => {
+  return <div className={s.message}>{props.message}</div>;
+};
 
 const Dialogs = () => {
-    return (
-      <div>
-      Dialogs
+  let dialogData = [
+    { id: 1, name: "Dima" },
+    { id: 2, name: "Sergey" },
+    { id: 3, name: "Andrey" },
+    { id: 4, name: "Sveta" },
+    { id: 5, name: "Valera" }
+  ];
+
+  let messagesData = [
+    { id: 1, message: "Укутна" },
+    { id: 1, message: "Дратути!" },
+    { id: 1, message: "Я пожилой енотик" },
+    { id: 1, message: "Волд оф тутлс" }
+  ];
+
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <DialogItem name="Dima" id="1" />
+        <DialogItem name="Sergey" id="2" />
+        <DialogItem name="Andrey" id="3" />
+        <DialogItem name="Sveta" id="4" />
       </div>
-            );
-  }
+      <div className={s.messages}>
+        <Message message="Укутна!" />
+        <Message message="Дратути!" />
+        <Message message="Дратути!" />
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -6,8 +6,7 @@ const ProfileInfo = () => {
     return (
         <div>
             <div className={s.item}>
-                <img
-                    src='https://avatars.mds.yandex.net/get-pdb/27625/7bb5cc58-a3b1-407c-a4ca-3be54a77d555/s1200'/>
+                <img src='https://avatars.mds.yandex.net/get-pdb/27625/7bb5cc58-a3b1-407c-a4ca-3be54a77d555/s1200'/>
             </div>
             <div className={s.descriptionBlock}>
                 ava + description
